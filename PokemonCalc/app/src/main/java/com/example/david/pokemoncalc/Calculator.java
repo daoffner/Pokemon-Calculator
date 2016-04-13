ca7921a32eb613db54e077b812b5b94b3b5ea4ce
daoffner <dpoffner@sbcglobal.net>
2016-04-12 23:35:46 -0500

    Added a background, and tried to add a home button.
    Button make the app crash so ill get rid of it. It isn't necessary.

--- PokemonCalc/app/src/main/java/com/example/david/pokemoncalc/Calculator.java
+++ PokemonCalc/app/src/main/java/com/example/david/pokemoncalc/Calculator.java
@@ -1,14 +1,17 @@
 package com.example.david.pokemoncalc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Calculator extends AppCompatActivity {
 
+    private Button homeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,14 @@ public class Calculator extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        homeButton = (Button) findViewById(R.id.home);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Calculator.this, Home.class));
+            }
+        });
+
     }
 
 }
