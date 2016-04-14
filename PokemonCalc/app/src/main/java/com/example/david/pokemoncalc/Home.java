47c8b0e7919d1b979ab3c8e157fffb09819333d8
daoffner <dpoffner@sbcglobal.net>
2016-04-14 12:47:47 -0500

    Fixed a null pointer exception when I deleted items in the last commit

--- PokemonCalc/app/src/main/java/com/example/david/pokemoncalc/Home.java
+++ PokemonCalc/app/src/main/java/com/example/david/pokemoncalc/Home.java
@@ -53,15 +53,6 @@ public class Home extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         calcButton = (Button) findViewById(R.id.Calc);
         calcButton.setOnClickListener(new View.OnClickListener() {
             @Override
