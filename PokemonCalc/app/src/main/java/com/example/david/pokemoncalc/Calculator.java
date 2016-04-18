dd87ef2e271c71aeda870cfec28acb42e7b2b116
daoffner <dpoffner@sbcglobal.net>
2016-04-17 23:56:10 -0500

    Added my calculator but there is an error.
    Hopefully I can fix it by deleting r.java. Thanks stack overflow

--- PokemonCalc/app/src/main/java/com/example/david/pokemoncalc/Calculator.java
+++ PokemonCalc/app/src/main/java/com/example/david/pokemoncalc/Calculator.java
@@ -8,10 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Calculator extends AppCompatActivity {
 
     private Button homeButton;
+    private Button calcButton;
+    private TextView temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +24,33 @@ public class Calculator extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        calcButton = (Button) findViewById(R.id.calc);
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                StatsCalculator calculator = new StatsCalculator();
+                int hp=calculator.calcHp(R.id.baseHp, R.id.evHp, R.id.ivHp, R.id.lvl);
+                int atk=calculator.calcStat(R.id.baseAtk, R.id.evAtk, R.id.ivAtk, R.id.lvl);
+                int def=calculator.calcStat(R.id.baseDef, R.id.evDef, R.id.ivDef, R.id.lvl);
+                int spAtk=calculator.calcStat(R.id.baseSpAtk, R.id.evSpAtk, R.id.ivSpAtk, R.id.lvl);
+                int spDef=calculator.calcStat(R.id.baseSpDef, R.id.evSpDef, R.id.ivSpDef, R.id.lvl);
+                int speed=calculator.calcStat(R.id.baseSpeed, R.id.evSpeed, R.id.ivSpeed, R.id.lvl);
+                temp= (TextView) findViewById(R.id.finalHp);
+                temp.setText(hp);
+                temp= (TextView) findViewById(R.id.finalAtk);
+                temp.setText(atk);
+                temp= (TextView) findViewById(R.id.finalDef);
+                temp.setText(def);
+                temp= (TextView) findViewById(R.id.finalSpAtk);
+                temp.setText(spAtk);
+                temp= (TextView) findViewById(R.id.finalSpDef);
+                temp.setText(spDef);
+                temp= (TextView) findViewById(R.id.finalSpeed);
+                temp.setText(speed);
+
+            }
+        });
         homeButton = (Button) findViewById(R.id.home);
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ PokemonCalc/app/src/main/java/com/example/david/pokemoncalc/StatsCalculator.java
@@ -0,0 +1,16 @@
+package com.example.david.pokemoncalc;
+
+public class StatsCalculator
+{
+	//int hp,atk,def,spAtk,spDef,speed,lvl;
+   //int evHp,evAtk,evDef,evSpAtk,evSpDef,evSpeed;
+   //int ivHp,ivAtk,ivDef,ivSpAtk,ivSpDef,ivSpeed;
+   public int calcHp(double base,double ev,double iv, double lvl)
+   {
+     return (int)((((2*base+iv+ev/4)*lvl)/100)+lvl+10);
+   }
+   public int calcStat(double base,double ev,double iv, double lvl)
+   {
+      return (int)((((2*base+iv+ev/4)*lvl)/100)+5);
+   }
+}
